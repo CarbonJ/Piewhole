4e47a504e4fbf27178bcbcfde05af48f2de02b37
CarbonJ <justin@Locke.local>
2015-07-29 20:03:06 -0400

    Added stubs for other views

--- piewhole/views.py
+++ piewhole/views.py
@@ -4,3 +4,19 @@ from flask import render_template
 @piewhole.route("/")
 def index():
     return render_template("intro.html")
+
+@piewhole.route("/login")
+def login():
+    return render_template("intro.html")
+
+@piewhole.route("/food")
+def fooddiary():
+    return render_template("intro.html")
+
+@piewhole.route("/weight")
+def weightinfo():
+    return render_template("intro.html")
+
+@piewhole.route("/profile")
+def userprofile():
+    return render_template("intro.html")
