39b03c3f35b321bee0d6c2e7d3ead8b068a9486c
CarbonJ <justin@Locke.local>
2015-07-30 18:07:48 -0400

    Working on login page and authentication.  Not returning ffailures yet

--- piewhole/login.py
+++ piewhole/login.py
@@ -7,8 +7,8 @@ from .models import User
 login_manager = LoginManager()
 login_manager.init_app(piewhole)
 
-login_manager.login_view = "login_get"
-login_manager.login_message_category = "danger"
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'danger'
 
 @login_manager.user_loader
 def load_user(id):
--- piewhole/views.py
+++ piewhole/views.py
@@ -1,6 +1,10 @@
 from piewhole import piewhole
 
+from .database import session
+from .models import User
+
 from flask import render_template
+from flask import request, redirect, url_for
 from flask import flash
 
 from flask.ext.login import login_user
@@ -9,15 +13,28 @@ from flask.ext.login import current_user
 from flask.ext.login import logout_user
 from werkzeug.security import check_password_hash
 
-from .models import User
+
+
 
 @piewhole.route("/")
 def index():
     return render_template("intro.html")
 
-@piewhole.route("/login")
+@piewhole.route("/login", methods=['GET'])
 def login():
     return render_template("login.html")
+
+@piewhole.route("/login", methods=['POST'])
+def login_post():
+    print(request.form['email'])
+    email = request.form['email']
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash('Incorrect user name or password', 'danger')
+        return redirect(url_for('login'))
+
+    login_user(user)
+    return redirect(url_for('index'))
 
 @piewhole.route("/food")
 @login_required
