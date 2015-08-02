5542dd54268c533cf75fd3787f5e0e107dc463a6
CarbonJ <justin@Locke.local>
2015-08-02 10:36:03 -0400

    Fixing Heroku

--- piewhole/config.py
+++ piewhole/config.py
@@ -5,6 +5,11 @@ class DevelopmentConfig(object):
     DEBUG = True
     SECRET_KEY = os.environ.get("PIEWHOLEKEY", "")
 
+class HerokuConfig(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://gtqwxmqbjngadn:gtqwxmqbjngadn@localhost:5432/d5272decs0ohn9'
+    DEBUG = True
+    SECRET_KEY = os.environ.get('JOLDERSHAMMER')
+
 class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://justin:justin@localhost:5432/piewhole_test'
     DEBUG = True
