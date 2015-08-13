ddecbe52831ebb57e02796329bfe2bb873cbe434
CarbonJ <justin@Locke.local>
2015-08-12 21:39:09 -0400

    Post mentor clean up

--- piewhole/views.py
+++ piewhole/views.py
@@ -146,77 +146,76 @@ def profile_post():
         print('POST WG: {}'.format(request.form['weightgoal']))
         print('POST HG: {}'.format(request.form['goodgoal']))
 
+        weightgoal = request.form['weightgoal']
+
         try:
-            weightgoal = request.form['weightgoal']
             goodgoal = (float(request.form['goodgoal']) * .01)
-
-            print('POST: Trying to make changes')
-            testweight = session.query(Goals).filter_by(user_id=current_user.id).first()
-
-            if not testweight:
-                if weightgoal and goodgoal > 0:
-                    print('POST: New goals, so inserting.')
-                    print('POST WG: {}'.format(weightgoal))
-                    print('POST HG: {}'.format(goodgoal))
-                    newgoals = Goals(user_id=current_user.id, weight_goal=weightgoal, health_goal=goodgoal)
-                    print(newgoals)
-                    session.add(newgoals)
-                    session.commit()
-                    print("POST: Commit done")
-                    return redirect(url_for('profile'))
-                else:
-                    flash('Need both a weight goal and health percentage.', 'danger')
-            else:
-                print('POST: Goals already exists, so updating.')
-                session.query(Goals).filter_by(user_id=current_user.id).update({"weight_goal": weightgoal})
-                session.query(Goals).filter_by(user_id=current_user.id).update({"health_goal": goodgoal})
-                session.commit()
         except (ValueError) as error:
             print('Failed: {}'.format(error))
+            flash('Need weight and health in number format.', 'danger')
+            return
+
+
+        print('POST: Trying to make changes')
+        testweight = session.query(Goals).filter_by(user_id=current_user.id).first()
+
+        if not testweight:
+            if weightgoal and goodgoal > 0:
+                print('POST: New goals, so inserting.')
+                print('POST WG: {}'.format(weightgoal))
+                print('POST HG: {}'.format(goodgoal))
+                newgoals = Goals(user_id=current_user.id, weight_goal=weightgoal, health_goal=goodgoal)
+                print(newgoals)
+                session.add(newgoals)
+                session.commit()
+                print("POST: Commit done")
+                return redirect(url_for('profile'))
+            else:
+                flash('Need both a weight goal and health percentage.', 'danger')
+        else:
+            print('POST: Goals already exists, so updating.')
+            session.query(Goals).filter_by(user_id=current_user.id).update({"weight_goal": weightgoal})
+            session.query(Goals).filter_by(user_id=current_user.id).update({"health_goal": goodgoal})
+            session.commit()
+
 
     def update_user():
         print('POST USER: {}'.format(current_user.username))
         print('POST USERFORM: {}'.format(request.form['username']))
         print('POST EMAILFORM: {}'.format(request.form['email']))
 
-        try:
-            username = request.form['username']
-            email = request.form['email']
+        username = request.form['username']
+        email = request.form['email']
 
-            emailtest = session.query(Users).filter_by(email=email).first()
+        emailtest = session.query(Users).filter_by(email=email).first()
 
-            if emailtest and email != current_user.email:
-                flash('That email address is already in use.', 'danger')
-            else:
-                print('POST: Trying to make changes')
-                session.query(Users).filter_by(id=current_user.id).update({"username": username})
-                session.query(Users).filter_by(id=current_user.id).update({"email": email})
-                session.commit()
-                print('POST: Changes commited')
-        except:
-            flash('NO GOOD EMAIL.', 'danger')
-            raise
+        if emailtest and email != current_user.email:
+            flash('That email address is already in use.', 'danger')
+        else:
+            print('POST: Trying to make changes')
+            session.query(Users).filter_by(id=current_user.id).update({"username": username})
+            session.query(Users).filter_by(id=current_user.id).update({"email": email})
+            session.commit()
+            print('POST: Changes commited')
 
     def update_password():
         print('POST USER: {}'.format(current_user.username))
-        try:
-            pwd = request.form['originalpassword']
-            pw1 = request.form['password1']
-            pw2 = request.form['password2']
 
-            user = session.query(Users).filter_by(id=current_user.id).first()
+        pwd = request.form['originalpassword']
+        pw1 = request.form['password1']
+        pw2 = request.form['password2']
 
-            if user and check_password_hash(user.password, pwd):
-                if pw1 == pw2:
-                    print('POST: New passwords good.')
-                    session.query(Users).filter_by(id=current_user.id).update({'password': generate_password_hash(pw1)})
-                    session.commit()
-                else:
-                    flash('The new passwords do not match, please try again.', 'warning')
+        user = session.query(Users).filter_by(id=current_user.id).first()
+
+        if user and check_password_hash(user.password, pwd):
+            if pw1 == pw2:
+                print('POST: New passwords good.')
+                session.query(Users).filter_by(id=current_user.id).update({'password': generate_password_hash(pw1)})
+                session.commit()
             else:
-                flash('Incorrect password.', 'danger')
-        except:
-            raise
+                flash('The new passwords do not match, please try again.', 'warning')
+        else:
+            flash('Incorrect password.', 'danger')
 
 
     if request.form['submit'] == 'user':
